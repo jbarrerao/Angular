401cc1bfed927583cd757503f881c1af66714a1e
Juan Pablo Barrera Ospina <juan_barrerao@satrack.com>
2019-09-19 13:32:03 -0500

    clase 19 septiembre

--- satrack-clients/src/app/authentication/authentication.module.ts
+++ satrack-clients/src/app/authentication/authentication.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AngularMaterialModule } from '../modules/angular-material/angular-material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     AngularMaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthenticationModule { }
--- satrack-clients/src/app/authentication/login/login.component.ts
+++ satrack-clients/src/app/authentication/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public user = new FormControl('', Validators.required);
+  public password = new FormControl('', Validators.required);
+
   constructor() { }
 
   ngOnInit() {
   }
-
+ 
+  login()
+  {
+    console.log(this.user.value);
+    console.log(this.password.value);
+  }
 }
